79e1f64639ad9d3b4aa86261e4a74373abbb9029
Dal Sal <ds@mbp-de-dal.home>
2023-06-14 17:38:06 +0200

    paramétrage flèche gauche et finalisation du carroussel fonctionnel

--- assets/script.js
+++ assets/script.js
@@ -40,17 +40,35 @@ window.addEventListener("load", () => {
     const rightArrow = document.querySelector(".arrow_right");
     const leftArrow = document.querySelector(".arrow_left");
 
+
+    // Paramétrage des évenements à effectuer au click de la flèche droite 
     rightArrow.addEventListener("click",() => {
         dots[carouselIdx].classList.remove("dot_selected"); // Retrait du dot selected en cours 
         carouselIdx++;
-        if (carouselIdx > 3) carouselIdx = 0 ; // paramétrage du nombres de dots selected 
+        if (carouselIdx > 3) carouselIdx = 0 ; // paramétrage du nombres de dots selected maximum
         dots[carouselIdx].classList.add("dot_selected"); // Ajout du dot selected au dot suivant 
 
 
         let slide = slides[carouselIdx] ; 
         let slideImg = slide.image ; 
         imageBanner.src = `./assets/images/slideshow/${slideImg}`; // Ajout du slideshow +1
-        tagLineBanner.innerHTML = tagLine; // Ajout du tagLine+1
-        } 
+        let tagLine = slide.tagLine ; 
+        tagLineBanner.innerHTML = tagLine; // Ajout du tagLine -1
+        }) 
+
+    // Paramétrage des évenements à effectuer au click de la flèche gauche 
+    leftArrow.addEventListener("click",() => {
+        dots[carouselIdx].classList.remove("dot_selected"); // Retrait du dot selected en cours 
+        carouselIdx--;
+        if (carouselIdx < 0) carouselIdx = 3 ; // paramétrage du nombres de dots selected minimum
+        dots[carouselIdx].classList.add("dot_selected"); // Ajout du dot selected au dot precedent
+    
+    
+        let slide = slides[carouselIdx] ; 
+        let slideImg = slide.image ; 
+        imageBanner.src = `./assets/images/slideshow/${slideImg}`; // Ajout du slideshow -1
+        let tagLine = slide.tagLine ; 
+        tagLineBanner.innerHTML = tagLine; // Ajout du tagLine -1
+        }
     
     )})
